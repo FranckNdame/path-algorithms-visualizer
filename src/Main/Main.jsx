0c10f4539472d37f7ed29cd300fdc4a3c1d42a47
FranckNdame <franck.mpouli@yahoo.com>
2019-10-06 22:54:21 +0100

    setup grid structure

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -5,14 +5,34 @@ import './Main.css'
 export default class Main extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            nodes: []
+        }
+    }
+
+    componentDidMount() {
+        const nodes = [];
+        for (let row = 0; row < 15; row++) {
+            const currentRow = [];
+            for (let col = 0; col < 50; col++) {
+                const currentNode = {
+                    col, row
+                }
+                currentRow.push(currentNode);
+            }
+            nodes.push(currentRow)
+        }
+        this.setState({nodes})
     }
 
     render() {
+        const { nodes } = this.state;
+        
         return(
-            <div>
-                Foo
-                <Node></Node>
+            <div className="grid">
+                {nodes.map((row, key) => {
+                    return <div>{row.map((node, nodeIdx) => <Node></Node>)}</div>
+                })}
             </div>
         )
     }
